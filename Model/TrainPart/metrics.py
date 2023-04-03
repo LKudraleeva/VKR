6ac8f08d75fcbbfac9053c4a8d96f828fa1463f1
LKudraleeva <kudraleeva_l@mail.ru>
2023-04-03 14:54:10 +0400

    Made structure

---
+++ Model/TrainPart/metrics.py
@@ -0,0 +1,18 @@
+from keras import backend as K
+
+SMOOTH = 1
+
+
+def dice_coefficient(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + SMOOTH) / (K.sum(y_true_f) + K.sum(y_pred_f) + SMOOTH)
+
+
+def dice_coefficient_loss(y_true, y_pred):
+    return -dice_coefficient(y_true, y_pred)
+
+
+def customized_loss(y_true, y_pred):
+    return (1*K.categorical_crossentropy(y_true, y_pred))+(0.5*dice_coefficient_loss(y_true, y_pred))
